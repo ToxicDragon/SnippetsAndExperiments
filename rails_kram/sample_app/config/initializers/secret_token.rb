62049e3e493d71a2e19890f3b674312e0895387a
bug <bug@bug-desktop.(none)>
2011-10-03 13:59:26 +0200

    starting sample rails app from rails tutorial

---
+++ rails_kram/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8df4193f31f43b605bbfc9fdaf41c91ae728065905da7adec8c70650d6e8bf5d20a9bbf04bcebab646e57bb39219c3cf825bba9d1b645923cdf2e42912705cf3'
