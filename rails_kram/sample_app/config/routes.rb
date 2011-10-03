96f60db6a99355e14489e99973a88db441a5ffee
bug <bug@bug-desktop.(none)>
2011-10-03 21:30:51 +0200

    routes stuff

--- rails_kram/sample_app/config/routes.rb
+++ rails_kram/sample_app/config/routes.rb
@@ -5,6 +5,8 @@ SampleApp::Application.routes.draw do
   
   get "pages/about"
   
+  root :to => 'pages#home'
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
