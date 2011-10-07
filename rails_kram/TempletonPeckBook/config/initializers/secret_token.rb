9511970d2a460d77c0812e29e6c2d2f5927d0c60
Chef font janze <spargulant@safe-mail.net>
2011-10-07 23:17:45 +0200

    created Templeton Peck Book

---
+++ rails_kram/TempletonPeckBook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TempletonPeckBook::Application.config.secret_token = '3829e52da473efefaa03980276a1ba80e5b10ebf822f6dff68029101316b5e3639b5122b828554cec3b7160179e1666a9eba3fb7399dc698c9992697f1ff2122'
